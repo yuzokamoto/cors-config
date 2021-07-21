0b6d14d230d62ae81037f16592817b1b2332ce52
yuzo <yuzokamoto@gmail.com>
2021-07-21 13:30:04 -0300

    vars config

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import express from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 app.use(express.json())
@@ -9,4 +12,4 @@ app.get("/", (req, res) => {
   res.send("Servidor online.")
 })
 
-app.listen(3003)
+app.listen(process.env.PORT)
